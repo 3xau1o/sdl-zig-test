b18253946a5e498a9859114d509e63088ff44e18
3xau1o <19735362+3xau1o@users.noreply.github.com>
2025-07-15 18:24:13 -0700

    .so

--- build.zig
+++ build.zig
@@ -1,11 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "hello",
         .root_source_file = b.path("src/main.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+        .target = target,
+        .optimize = optimize,
     });
 
     exe.linkLibC();
@@ -14,8 +17,15 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(exe);
 
-    const run_exe = b.addRunArtifact(exe);
+    // build shared library from audio.zig
 
-    const run_step = b.step("run", "Run the application");
-    run_step.dependOn(&run_exe.step);
+    const lib_aud = b.addSharedLibrary(.{
+        .name = "aud",
+        .root_source_file = b.path("src/audio.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    lib_aud.linkLibC();
+    lib_aud.linkSystemLibrary("SDL3");
+    b.installArtifact(lib_aud);
 }
